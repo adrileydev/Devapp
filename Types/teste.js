b24a73a66a7541661909ccd0433fdd542b5090b6
adriley@outlook.com <adriley@outlook.com>
2017-06-01 13:46:03 -0300

    este type

---
+++ Types/teste.js
@@ -0,0 +1,8 @@
+var ver = (function () {
+    function ver() {
+    }
+    return ver;
+}());
+var v = new ver();
+v.nome = "ADRILEY FRANCISCO ALMEIDA PEREIRA";
+console.log(v.nome);
---
+++ Types/teste.ts
@@ -0,0 +1,6 @@
+class ver{
+   nome : string;    
+} 
+ let v = new ver();
+ v.nome = "ADRILEY FRANCISCO ALMEIDA PEREIRA";
+console.log(v.nome);

5d4f3cfef191b6c50a3a347ad8ef757640db46cd
adriley <adriley@outlook.com>
2017-06-27 13:06:00 -0300

    Fase 4
    fase 4

--- meat/src/app/app.routes.ts
+++ meat/src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from "@angular/router";
-
 import { HomeComponent } from "app/home/home.component";
 import { AboutComponent } from "app/about/about.component";
 import { RestauranteComponent } from "app/restaurante/restaurante.component";
 import { PedidoComponent } from "app/pedido/pedido.component";
 
 export const ROUTES: Routes = [
-    {path:'',component :HomeComponent },
-    {path:'About',component :AboutComponent },
+     {path:'',component :HomeComponent },
+     {path:'About',component :AboutComponent },
      {path:'restaurante',component :RestauranteComponent },
      {path:'Pedido',component :PedidoComponent }
 
--- meat/src/app/home/home.component.ts
+++ meat/src/app/home/home.component.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mt-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './home.component.html'
+  
 })
 export class HomeComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+} 
